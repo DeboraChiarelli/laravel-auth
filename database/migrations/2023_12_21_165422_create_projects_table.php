9d0895092ca7ff8e4f33fe32784c9e8f3cb74251
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-12-21 18:30:26 +0100

    Fix - migration

--- database/migrations/2023_12_21_165422_create_projects_table.php
+++ database/migrations/2023_12_21_165422_create_projects_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->unique();
             $table->string('image_path'); 
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
